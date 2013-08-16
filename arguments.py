12d5bbb61e07ed6387dddf42c1c52515bf1b8013
SimonE <si_sitesit@yahoo.co.nz>
2013-08-17 00:47:54 +1200

    Added RandomAntiSocial and FairHunterAntiSocial player classes.

--- arguments.py
+++ arguments.py
@@ -29,7 +29,30 @@ def get_arguments():
 
     For help, run `python app.py -h` or `python app.py --help`
     '''
-    parser = ArgumentParser()
+    parser = ArgumentParser(description="Tournament app for testing the " \
+        "player classes created for the brilliant.org Hunger Games games " \
+        "theory programming tournament.", 
+                            epilog="Multiple bots of the same type can " \
+                                "be created with different arguments by " \
+                                "separating the argument groups with " \
+                                "spaces. eg --random 3,0.5,0 2,0.8,0")
+
+    game_options = parser.add_argument_group("game options")
+    game_options.add_argument("-q", "--quiet", dest="verbose",
+                        default=not DEFAULT_VERBOSITY, action="store_false",
+                        help="use quiet output (off by default)")
+    game_options.add_argument("-l", "--min-rounds", dest="min_rounds",
+                        default=DEFAULT_MIN_ROUNDS, type=int,
+                        help="the minimum number of rounds to play")
+    game_options.add_argument("-x", "--average-rounds", dest="average_rounds",
+                        default=DEFAULT_AVERAGE_ROUNDS, type=int,
+                        help="the average number of rounds to play")
+    game_options.add_argument("-e", "--end-early", dest="end_early",
+                        default=DEFAULT_END_EARLY, action="store_true",
+                        help="end the game if 'Player' is eliminated")
+    game_options.add_argument("-lf", "--log-filename", dest="log_filename",
+                        default=DEFAULT_LOG_FILE, 
+                        help="name of output file")
 
     bot_options = parser.add_argument_group("bots to use for game")    
     bot_options.add_argument("-p", "--pushover", dest="pushover",
@@ -81,7 +104,7 @@ def get_arguments():
                             "logging or not, in the form  " \
                             "'number,p_hunt,do_log' where number is an int, " \
                             "p_hunt is a float in the range 0-1, and " \
-                            "do_log is 0 or 1")
+                            "do_log is 0 or 1.")
     bot_options.add_argument("-bh", "--bounded-hunter", dest="boundedhunter",
                         default=[], nargs="*",
                         help="the number and limits of BoundedHunter bots to play " \
@@ -99,23 +122,27 @@ def get_arguments():
                             "where number is an int, threshold is a float " \
                             "in the range 0-1, first_hunt_value is 'h' or 's' " \
                             "and do_log is 0 or 1")
-
-    game_options= parser.add_argument_group("game options")
-    game_options.add_argument("-q", "--quiet", dest="verbose",
-                        default=not DEFAULT_VERBOSITY, action="store_false",
-                        help="use quiet output (off by default)")
-    game_options.add_argument("-l", "--min-rounds", dest="min_rounds",
-                        default=DEFAULT_MIN_ROUNDS, type=int,
-                        help="the minimum number of rounds to play")
-    game_options.add_argument("-x", "--average-rounds", dest="average_rounds",
-                        default=DEFAULT_AVERAGE_ROUNDS, type=int,
-                        help="the average number of rounds to play")
-    game_options.add_argument("-e", "--end-early", dest="end_early",
-                        default=DEFAULT_END_EARLY, action="store_true",
-                        help="end the game if 'Player' is eliminated")
-    game_options.add_argument("-lf", "--log-filename", dest="log_filename",
-                        default=DEFAULT_LOG_FILE, 
-                        help="name of output file")
+    bot_options.add_argument("-ras", "--random-antisocial", dest="random_antisocial",
+                        default=[], nargs="*",
+                        help="the number and probabilities of RandomAntiSocial bots to play " \
+                            "with, and whether they require detailed " \
+                            "logging or not, in the form  " \
+                            "'number,p_hunt,antisocial_threshold,evil_threshold,do_log' " \
+                            "where number is an int, " \
+                            "p_hunt, antisocial_threshold and evil_threshold " \
+                            "are floats in the range 0-1, " \
+                            "and do_log is 0 or 1.")
+    bot_options.add_argument("-fhas", "--fair-hunter-antisocial", dest="fairhunter_antisocial",
+                        default=[], nargs="*",
+                        help="the number and probabilities of " \
+                            "FairHunterAntiSocial bots to play " \
+                            "with, and whether they require detailed " \
+                            "logging or not, in the form  " \
+                            "'number,antisocial_threshold,evil_threshold,do_log' " \
+                            "where number is an int, " \
+                            "antisocial_threshold and evil_threshold " \
+                            "are floats in the range 0-1, " \
+                            "and do_log is 0 or 1.")
     
     args = parser.parse_args()
 
@@ -175,7 +202,40 @@ def get_arguments():
         elif number == 1:
             bots.append(FixedThreshold(threshold, first_hunt_value, None, do_log))
         else:
-            bots.extend([FixedThreshold(threshold, first_hunt_value, i, do_log) for i in range(number)])
+            bots.extend([FixedThreshold(threshold, first_hunt_value, i, do_log) for i in range(number)]) 
+
+    for x in args.random_antisocial:
+        (number, p_hunt, antisocial_threshold, evil_threshold, do_log) = x.split(",")
+        number = int(number)
+        p_hunt = float(p_hunt)
+        antisocial_threshold = float(antisocial_threshold)
+        evil_threshold = float(evil_threshold)
+        do_log = int(do_log)
+        
+        if number <= 0:
+            continue
+        elif number == 1:
+            bots.append(RandomAntiSocial(p_hunt, antisocial_threshold, 
+                                         evil_threshold, None, do_log))
+        else:
+            bots.extend([RandomAntiSocial(p_hunt, antisocial_threshold, 
+                                          evil_threshold, i, do_log) for i in range(number)])
+                                          
+    for x in args.fairhunter_antisocial:
+        (number, antisocial_threshold, evil_threshold, do_log) = x.split(",")
+        number = int(number)
+        antisocial_threshold = float(antisocial_threshold)
+        evil_threshold = float(evil_threshold)
+        do_log = int(do_log)
+        
+        if number <= 0:
+            continue
+        elif number == 1:
+            bots.append(FairHunterAntiSocial(antisocial_threshold, 
+                                         evil_threshold, None, do_log))
+        else:
+            bots.extend([FairHunterAntiSocial(antisocial_threshold, 
+                                          evil_threshold, i, do_log) for i in range(number)])
         
     players = bots if bots else DEFAULT_PLAYERS       
         
